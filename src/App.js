be8d0687b89654d77b34d27b88c65bba4008632f
mansour.malloum <mansourmalloum@gmail.com>
2020-01-28 10:42:59 +0100

    Header component have being created and styled

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage.component';
 import ShopPage from './pages/shop/shopPage.component.jsx';
-
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop' component={ShopPage}/>
-    </Switch>
-  );
+    <div>
+     <Header/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
+    </div> 
+  )
 }
 
 export default App;
